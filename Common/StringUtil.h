938e5aade8c2725dd0983a6efb70b79bdf72566b
xtne6f <xt4ubq@gmail.com>
2013-05-04 18:47:59 +0900

    StringUtil.cppを少し効率化

--- Common/StringUtil.h
+++ Common/StringUtil.h
@@ -7,21 +7,23 @@ void Format(string& strBuff, const char *format, ...);
 
 void Format(wstring& strBuff, const WCHAR *format, ...);
 
-void Replace(string& strBuff, const string strOld, const string strNew);
+void Replace(string& strBuff, const string& strOld, const string& strNew); //alias-safe
 
-void Replace(wstring& strBuff, const wstring strOld, const wstring strNew);
+void Replace(wstring& strBuff, const wstring& strOld, const wstring& strNew); //alias-safe
 
-void WtoA(wstring strIn, string& strOut);
+void WtoA(const wstring& strIn, string& strOut);
 
-void WtoUTF8(wstring strIn, string& strOut);
+void WtoUTF8(const wstring& strIn, string& strOut);
 
-void AtoW(string strIn, wstring& strOut);
+void AtoW(const string& strIn, wstring& strOut);
 
-void UTF8toW(string strIn, wstring& strOut);
+void UTF8toW(const string& strIn, wstring& strOut);
 
-BOOL Separate(string strIn, string strSep, string& strLeft, string& strRight);
+BOOL Separate(const string& strIn, const char *sep, string& strLeft, string& strRight); //alias-safe
+inline BOOL Separate(const string& strIn, const string& strSep, string& strLeft, string& strRight){ return Separate(strIn, strSep.c_str(), strLeft, strRight); }
 
-BOOL Separate(wstring strIn, wstring strSep, wstring& strLeft, wstring& strRight);
+BOOL Separate(const wstring& strIn, const WCHAR *sep, wstring& strLeft, wstring& strRight); //alias-safe
+inline BOOL Separate(const wstring& strIn, const wstring& strSep, wstring& strLeft, wstring& strRight){ return Separate(strIn, strSep.c_str(), strLeft, strRight); }
 
 void ChkFolderPath(string& strPath);
 
@@ -31,9 +33,9 @@ void ChkFileName(string& strPath);
 
 void ChkFileName(wstring& strPath);
 
-int CompareNoCase(string str1, string str2);
+int CompareNoCase(const string& str1, const string& str2);
 
-int CompareNoCase(wstring str1, wstring str2);
+int CompareNoCase(const wstring& str1, const wstring& str2);
 
 BOOL UrlDecode(LPCSTR src, DWORD srcSize, string& dest);
 
@@ -43,9 +45,9 @@ void Trim(string& strBuff);
 
 void Trim(wstring& strBuff);
 
-string Tolower(string src);
+string Tolower(const string& src);
 
-wstring Tolower(wstring src);
+wstring Tolower(const wstring& src);
 
 void EscapeXmlString(wstring& src);
 
