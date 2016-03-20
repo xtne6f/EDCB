c16fc75fecb1ce8f71c6e16054b5a0aa148129c9
tkntrec <Tokinata@gmail.com>
2016-03-21 00:42:37 +0900

    28時間表示の表示ミスを修正(2ch_EDCB/49/367)
    5120b05cdfe88d52519706bd54707bcd3dae644b のエンバグ。
    終了時刻が24時を超えているとき、開始時刻と同じ「分:秒」が表示されていた。
    ※表示のみのミスで、予約自体は正しく行われていました。

--- EpgTimer/EpgTimer/Common/CommonManagerClass.cs
+++ EpgTimer/EpgTimer/Common/CommonManagerClass.cs
@@ -704,8 +704,8 @@ namespace EpgTimer
             if (Settings.Instance.LaterTimeUse == true)
             {
                 var time28 = new DateTime28(time, isUse28, ref_start);
-                return time28.DateTimeMod.ToString((isNoDay == true ? "" : (isNoYear == true ? "MM/dd(ddd) " : "yyyy/MM/dd(ddd) "))
-                 + time28.HourMod.ToString("00:") + (isNoSecond == true ? "mm" : "mm:ss"));
+                return (isNoDay == true ? "" : time28.DateTimeMod.ToString((isNoYear == true ? "MM/dd(ddd) " : "yyyy/MM/dd(ddd) ")))
+                + time28.HourMod.ToString("00:") + time.ToString(isNoSecond == true ? "mm" : "mm:ss");
             }
             else
             {
