d806a120ad5670134fa3f8d65e595cbed34d377d
xtne6f <xt4ubq@gmail.com>
2016-04-17 13:17:52 +0900

    HTTPサーバのスレッド数のデフォルトを3→5に上げる

--- EpgTimerSrv/EpgTimerSrv/EpgTimerSrvMain.cpp
+++ EpgTimerSrv/EpgTimerSrv/EpgTimerSrvMain.cpp
@@ -691,7 +691,7 @@ void CEpgTimerSrvMain::ReloadNetworkSetting()
 		}
 		this->httpOptions.accessControlList = GetPrivateProfileToString(L"SET", L"HttpAccessControlList", L"+127.0.0.1", iniPath.c_str());
 		this->httpOptions.authenticationDomain = GetPrivateProfileToString(L"SET", L"HttpAuthenticationDomain", L"", iniPath.c_str());
-		this->httpOptions.numThreads = GetPrivateProfileInt(L"SET", L"HttpNumThreads", 3, iniPath.c_str());
+		this->httpOptions.numThreads = GetPrivateProfileInt(L"SET", L"HttpNumThreads", 5, iniPath.c_str());
 		this->httpOptions.requestTimeout = GetPrivateProfileInt(L"SET", L"HttpRequestTimeoutSec", 120, iniPath.c_str()) * 1000;
 		this->httpOptions.sslProtocolVersion = GetPrivateProfileInt(L"SET", L"HttpSslProtocolVersion", 2, iniPath.c_str());
 		this->httpOptions.keepAlive = GetPrivateProfileInt(L"SET", L"HttpKeepAlive", 0, iniPath.c_str()) != 0;
