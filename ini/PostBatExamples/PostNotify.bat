59b36a2e8dd7a0daca310e325cf9ef245c155e08
xtne6f <xt4ubq@gmail.com>
2016-05-03 00:57:10 +0900

    EdcbSchUploaderを調整
    しょぼかるのHTTPS対応は復活させる予定があるのかも分からないのでHTTPにする
    CSCによる自動コンパイルは若干分かりづらいのでやめる

--- ini/PostBatExamples/PostNotify.bat
+++ ini/PostBatExamples/PostNotify.bat
@@ -3,33 +3,14 @@ rem 予約情報更新のタイミングでしょぼいカレンダーに予約をアップロードするサンプル
 rem _EDCBX_DIRECT_
 rem _EDCBX_#HIDE_ (#を取り除くとウィンドウ非表示になる)
 
-rem v3.5以上のC#コンパイラのパスを指定する
-set CSC_PATH=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc.exe
-
 rem 直接実行か予約情報更新のときだけ
 if not defined NotifyID set NotifyID=2
 if "%NotifyID%"=="2" (
-  if not exist EdcbSchUploader.exe (
-    if not exist EdcbSchUploader.cs (
-      echo エラー: EdcbSchUploader.cs が見つかりません。
-      goto EOF
-    )
-    if not exist %CSC_PATH% (
-      echo エラー: %CSC_PATH% が見つかりません。
-      goto EOF
-    )
-    echo EdcbSchUploader.cs をコンパイルします。
-    %CSC_PATH% /nologo /optimize EdcbSchUploader.cs
-    if not exist EdcbSchUploader.exe (
-      echo エラー: EdcbSchUploader.exe が見つかりません。
-      goto EOF
-    )
-  )
-
   echo しょぼいカレンダーに予約をアップロードします。
+  rem デフォルトの送信先は http://cal.syoboi.jp/sch_upload 。HTTPSを使う場合は↓のようにする
+  rem EdcbSchUploader.exe "user" "pass" "" 0 "https://cal.syoboi.jp/sch_upload"
   EdcbSchUploader.exe 【ユーザIDとパスワードをここに指定】
 
-:EOF
   rem 実戦投入時はこのpauseを取り除く
   pause
 )
