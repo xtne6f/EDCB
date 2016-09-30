6f0d2d02b69071f2af5e8fac627c8262243ba9cf
xtne6f <xt4ubq@gmail.com>
2016-09-30 19:27:03 +0900

    未操作時間取得の整理と修正
    Environment.TickCountがGetTickCount()であるとは保証されないと思うので、念のため
    TickCountの取得位置や計算方法があまりよろしくない等の細かい修正

--- EpgTimer/EpgTimer/MainWindow.xaml.cs
+++ EpgTimer/EpgTimer/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading; //紅
-using System.Windows.Interop; //紅
-using System.Runtime.InteropServices; //紅
 
 namespace EpgTimer
 {
@@ -1201,39 +1199,13 @@ namespace EpgTimer
             }
         }
 
-        internal struct LASTINPUTINFO
-        {
-            public uint cbSize;
-            public uint dwTime;
-        }
-
-        [DllImport("User32.dll")]
-        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
-
-        [DllImport("Kernel32.dll")]
-        public static extern UInt32 GetTickCount();
-
         private void ShowSleepDialog(UInt16 param)
         {
-            LASTINPUTINFO info = new LASTINPUTINFO();
-            info.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(info);
-            GetLastInputInfo(ref info);
-
-            // 現在時刻取得
-            UInt64 dwNow = GetTickCount();
-
-            // GetTickCount()は49.7日周期でリセットされるので桁上りさせる
-            if (info.dwTime > dwNow)
-            {
-                dwNow += 0x100000000;
-            }
-
             if (IniFileHandler.GetPrivateProfileInt("NO_SUSPEND", "NoUsePC", 0, SettingPath.TimerSrvIniPath) == 1)
             {
-                UInt32 ngUsePCTime = (UInt32)IniFileHandler.GetPrivateProfileInt("NO_SUSPEND", "NoUsePCTime", 3, SettingPath.TimerSrvIniPath);
-                UInt32 threshold = ngUsePCTime * 60 * 1000;
+                int ngUsePCTime = IniFileHandler.GetPrivateProfileInt("NO_SUSPEND", "NoUsePCTime", 3, SettingPath.TimerSrvIniPath);
 
-                if (ngUsePCTime == 0 || dwNow - info.dwTime < threshold)
+                if (ngUsePCTime == 0 || CommonUtil.GetIdleTimeSec() < ngUsePCTime * 60)
                 {
                     return;
                 }
