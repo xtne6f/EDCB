bf29144223f47ae7b4df8d5cec4d6cfa08614faf
xtne6f <xt4ubq@gmail.com>
2017-01-27 19:37:22 +0900

    Luaスクリプトの予約設定フォームをEpgTimerのデザインに寄せる

--- ini/HttpPublic/legacy/util.lua
+++ ini/HttpPublic/legacy/util.lua
@@ -54,7 +54,7 @@ function RecSettingTemplate(rs)
     ..'<option value="3"'..(rs.recMode==3 and ' selected="selected"' or '')..'>指定サービスのみ（デコード処理なし）'
     ..'<option value="4"'..(rs.recMode==4 and ' selected="selected"' or '')..'>視聴'
     ..'<option value="5"'..(rs.recMode==5 and ' selected="selected"' or '')..'>無効</select><br>\n'
-    ..'追従: <select name="tuijyuuFlag">'
+    ..'イベントリレー追従: <select name="tuijyuuFlag">'
     ..'<option value="0"'..(not rs.tuijyuuFlag and ' selected="selected"' or '')..'>しない'
     ..'<option value="1"'..(rs.tuijyuuFlag and ' selected="selected"' or '')..'>する</select><br>\n'
     ..'優先度: <select name="priority">'
@@ -66,21 +66,21 @@ function RecSettingTemplate(rs)
     ..'ぴったり（？）録画: <select name="pittariFlag">'
     ..'<option value="0"'..(not rs.pittariFlag and ' selected="selected"' or '')..'>しない'
     ..'<option value="1"'..(rs.pittariFlag and ' selected="selected"' or '')..'>する</select><br>\n'
-    ..'録画後動作: <select name="suspendMode">'
-    ..'<option value="0"'..(rs.suspendMode==0 and ' selected="selected"' or '')..'>デフォルト設定を使用'
-    ..'<option value="1"'..(rs.suspendMode==1 and ' selected="selected"' or '')..'>スタンバイ'
-    ..'<option value="2"'..(rs.suspendMode==2 and ' selected="selected"' or '')..'>休止'
-    ..'<option value="3"'..(rs.suspendMode==3 and ' selected="selected"' or '')..'>シャットダウン'
-    ..'<option value="4"'..(rs.suspendMode==4 and ' selected="selected"' or '')..'>何もしない</select>\n'
-    ..'<input type="checkbox" name="rebootFlag" value="1"'..(rs.rebootFlag and ' checked="checked"' or '')..'>復帰後再起動する<br>\n'
-    ..'録画マージン: <input type="checkbox" name="useDefMarginFlag" value="1"'..(rs.startMargin and '' or ' checked="checked"')..'>デフォルト設定で使用\n'
-    ..'開始<input type="text" name="startMargin" value="'..(rs.startMargin or 0)..'">\n'
-    ..'終了<input type="text" name="endMargin" value="'..(rs.endMargin or 0)..'"><br>\n'
-    ..'指定サービス対象データ: '
-    ..'<input type="checkbox" name="serviceMode" value="0"'..(rs.serviceMode%2==0 and ' checked="checked"' or '')..'>デフォルト設定で使用'
-    ..'<input type="checkbox" name="serviceMode_1" value="0"'..(rs.serviceMode%2~=0 and math.floor(rs.serviceMode/16)%2~=0 and ' checked="checked"' or '')..'>字幕データを含める'
+    ..'録画マージン: <input type="checkbox" name="useDefMarginFlag" value="1"'..(rs.startMargin and '' or ' checked="checked"')..'>デフォルト || '
+    ..'開始（秒） <input type="text" name="startMargin" value="'..(rs.startMargin or 0)..'"> '
+    ..'終了（秒） <input type="text" name="endMargin" value="'..(rs.endMargin or 0)..'"><br>\n'
+    ..'指定サービス対象データ: <input type="checkbox" name="serviceMode" value="0"'..(rs.serviceMode%2==0 and ' checked="checked"' or '')..'>デフォルト || '
+    ..'<input type="checkbox" name="serviceMode_1" value="0"'..(rs.serviceMode%2~=0 and math.floor(rs.serviceMode/16)%2~=0 and ' checked="checked"' or '')..'>字幕を含める '
     ..'<input type="checkbox" name="serviceMode_2" value="0"'..(rs.serviceMode%2~=0 and math.floor(rs.serviceMode/32)%2~=0 and ' checked="checked"' or '')..'>データカルーセルを含める<br>\n'
-    ..'連続録画動作: '
+    ..'<table><tr><td>録画フォルダ</td><td>出力PlugIn</td><td>ファイル名PlugIn</td><td>部分受信</td></tr>\n'
+  for i,v in ipairs(rs.recFolderList) do
+    s=s..'<tr><td>'..v.recFolder..'</td><td>'..v.writePlugIn..'</td><td>'..v.recNamePlugIn..'</td><td>いいえ</td></tr>\n'
+  end
+  for i,v in ipairs(rs.partialRecFolder) do
+    s=s..'<tr><td>'..v.recFolder..'</td><td>'..v.writePlugIn..'</td><td>'..v.recNamePlugIn..'</td><td>はい</td></tr>\n'
+  end
+  s=s..'</table>（プリセットによる変更のみ対応）<br>\n'
+    ..'<input type="checkbox" name="partialRecFlag" value="1"'..(rs.partialRecFlag~=0 and ' checked="checked"' or '')..'>部分受信（ワンセグ）を別ファイルに同時出力する<br>\n'
     ..'<input type="checkbox" name="continueRecFlag" value="1"'..(rs.continueRecFlag and ' checked="checked"' or '')..'>後ろの予約を同一ファイルで出力する<br>\n'
     ..'使用チューナー強制指定: <select name="tunerID"><option value="0"'..(rs.tunerID==0 and ' selected="selected"' or '')..'>自動'
   local a=edcb.GetTunerReserveAll()
@@ -88,20 +88,14 @@ function RecSettingTemplate(rs)
     s=s..'<option value="'..a[i].tunerID..'"'..(a[i].tunerID==rs.tunerID and ' selected="selected"' or '')..string.format('>ID:%08X(', a[i].tunerID)..a[i].tunerName..')'
   end
   s=s..'</select><br>\n'
-    ..'録画後実行bat（プリセットによる変更のみ対応）: '..rs.batFilePath..'<br>\n'
-    ..'録画フォルダ、使用プラグイン（プリセットによる変更のみ対応）:<table>'
-    ..'<tr><td>フォルダ</td><td>出力PlugIn</td><td>ファイル名PlugIn</td></tr>\n'
-  for i,v in ipairs(rs.recFolderList) do
-    s=s..'<tr><td>'..v.recFolder..'</td><td>'..v.writePlugIn..'</td><td>'..v.recNamePlugIn..'</td></tr>\n'
-  end
-  s=s..'</table>'
-    ..'部分受信サービス:<br><input type="checkbox" name="partialRecFlag" value="1"'..(rs.partialRecFlag~=0 and ' checked="checked"' or '')..'>別ファイルに同時出力する<br>\n'
-    ..'録画フォルダ、使用プラグイン（プリセットによる変更のみ対応）:<table>'
-    ..'<tr><td>フォルダ</td><td>出力PlugIn</td><td>ファイル名PlugIn</td></tr>\n'
-  for i,v in ipairs(rs.partialRecFolder) do
-    s=s..'<tr><td>'..v.recFolder..'</td><td>'..v.writePlugIn..'</td><td>'..v.recNamePlugIn..'</td></tr>\n'
-  end
-  s=s..'</table><br>\n'
+    ..'録画後動作: <select name="suspendMode">'
+    ..'<option value="0"'..(rs.suspendMode==0 and ' selected="selected"' or '')..'>デフォルト設定を使用'
+    ..'<option value="1"'..(rs.suspendMode==1 and ' selected="selected"' or '')..'>スタンバイ'
+    ..'<option value="2"'..(rs.suspendMode==2 and ' selected="selected"' or '')..'>休止'
+    ..'<option value="3"'..(rs.suspendMode==3 and ' selected="selected"' or '')..'>シャットダウン'
+    ..'<option value="4"'..(rs.suspendMode==4 and ' selected="selected"' or '')..'>何もしない</select> '
+    ..'<input type="checkbox" name="rebootFlag" value="1"'..(rs.rebootFlag and ' checked="checked"' or '')..'>復帰後再起動する<br>\n'
+    ..'録画後実行bat（プリセットによる変更のみ対応）: '..(#rs.batFilePath==0 and '（なし）' or rs.batFilePath)..'<br>\n'
   return s
 end
 
