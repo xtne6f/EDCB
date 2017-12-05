21692f5d3ece522851647ff1b5b0057d3a6231a9
xtne6f <xt4ubq@gmail.com>
2017-12-05 22:21:52 +0900

    ビルド方法を更新

---
+++ ConvToSJIS.bat
@@ -0,0 +1,6 @@
+@echo This script converts *.h and *.cpp to Shift_JIS encoding.
+@pause
+cd /d "%~dp0"
+powershell -NoProfile -Command "&{ls -Recurse -Include *.h,*.cpp -Exclude resource.h,targetver.h|foreach{$_.FullName;$r=New-Object IO.StreamReader($_,[Text.Encoding]::GetEncoding(932));$s=$r.ReadToEnd();$r.Close();$w=New-Object IO.StreamWriter($_,$false,[Text.Encoding]::GetEncoding(932));$w.Write($s);$w.Close()}}"
+@echo Done.
+@pause
