928dd8852a3497e3d1a8bd20a49d1856f708adc7
xtne6f <xt4ubq@gmail.com>
2018-01-06 23:36:53 +0900

    ビルド方法などを更新

--- ini/PostBatExamples/PostNotify.bat
+++ ini/PostBatExamples/PostNotify.bat
@@ -3,15 +3,11 @@ rem 予約情報更新のタイミングでしょぼいカレンダーに予約をアップロードするサンプル
 rem _EDCBX_DIRECT_
 rem _EDCBX_#HIDE_ (#を取り除くとウィンドウ非表示になる)
 
-rem 常にx86版のpowershellを使用する(EpgTimer.exeのアーキテクチャに合わせる)
-set PsPath="powershell"
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set PsPath="%SystemRoot%\SysWOW64\WindowsPowerShell\v1.0\powershell"
-
 rem 直接実行か予約情報更新のときだけ
 if not defined NotifyID set NotifyID=2
 if "%NotifyID%"=="2" (
   echo しょぼいカレンダーに予約をアップロードします。
-  %PsPath% -NoProfile -ExecutionPolicy RemoteSigned -File ".\EdcbSchUploader.ps1" 【ユーザIDとパスワードをここに指定】
+  powershell -NoProfile -ExecutionPolicy RemoteSigned -File ".\EdcbSchUploader.ps1" 【ユーザIDとパスワードをここに指定】
 
   rem 実戦投入時はこのpauseを取り除く
   pause
