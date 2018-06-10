ed06dea7af30f956de03d68434827a97dddd2587
xtne6f <xt4ubq@gmail.com>
2018-06-10 13:28:26 +0900

    Write_PlugInの宣言や説明のクローンを減らす
    CWritePlugInUtilについてextern名と被らないようメソッド名を変える

--- Common/WritePlugInUtil.h
+++ Common/WritePlugInUtil.h
@@ -1,102 +1,5 @@
 #pragma once
 
-#include <Windows.h>
-
-//PlugInの名前を取得する
-//nameがNULL時は必要なサイズをnameSizeで返す
-//通常nameSize=256で呼び出し
-//戻り値
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// name						[OUT]名称
-// nameSize					[IN/OUT]nameのサイズ(WCHAR単位)
-typedef BOOL (WINAPI* GetPlugInNameWP)(
-	WCHAR* name,
-	DWORD* nameSize
-	);
-
-//PlugInで設定が必要な場合、設定用のダイアログなどを表示する
-//引数：
-// parentWnd				[IN]親ウインドウ
-typedef void (WINAPI* SettingWP)(
-	HWND parentWnd
-	);
-
-//複数保存対応のためインスタンスを新規に作成する
-//複数対応できない場合はこの時点でエラーとする
-//戻り値
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// id				[OUT]識別ID
-typedef BOOL (WINAPI* CreateCtrlWP)(
-	DWORD* id
-	);
-
-//インスタンスを削除する
-//戻り値
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// id				[IN]識別ID
-typedef BOOL (WINAPI* DeleteCtrlWP)(
-	DWORD id
-	);
-
-//ファイル保存を開始する
-//戻り値：
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// id					[IN]識別ID
-// fileName				[IN]保存ファイルフルパス（必要に応じて拡張子変えたりなど行う）
-// overWriteFlag		[IN]同一ファイル名存在時に上書きするかどうか（TRUE：する、FALSE：しない）
-// createSize			[IN]入力予想容量（188バイトTSでの容量。即時録画時など総時間未定の場合は0。延長などの可能性もあるので目安程度）
-typedef BOOL (WINAPI* StartSaveWP)(
-	DWORD id,
-	LPCWSTR fileName,
-	BOOL overWriteFlag,
-	ULONGLONG createSize
-	);
-
-//ファイル保存を終了する
-//戻り値：
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// id					[IN]識別ID
-typedef BOOL (WINAPI* StopSaveWP)(
-	DWORD id
-	);
-
-//実際に保存しているファイルパスを取得する（再生やバッチ処理に利用される）
-//filePathがNULL時は必要なサイズをfilePathSizeで返す
-//通常filePathSize=512で呼び出し
-//戻り値：
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// id					[IN]識別ID
-// filePath				[OUT]保存ファイルフルパス
-// filePathSize			[IN/OUT]filePathのサイズ(WCHAR単位)
-typedef BOOL (WINAPI* GetSaveFilePathWP)(
-	DWORD id,
-	WCHAR* filePath,
-	DWORD* filePathSize
-	);
-
-//保存用TSデータを送る
-//空き容量不足などで書き出し失敗した場合、writeSizeの値を元に
-//再度保存処理するときの送信開始地点を決める
-//戻り値：
-// TRUE（成功）、FALSE（失敗）
-//引数：
-// id					[IN]識別ID
-// data					[IN]TSデータ
-// size					[IN]dataのサイズ
-// writeSize			[OUT]保存に利用したサイズ
-typedef BOOL (WINAPI* AddTSBuffWP)(
-	DWORD id,
-	BYTE* data,
-	DWORD size,
-	DWORD* writeSize
-	);
-
 class CWritePlugInUtil
 {
 public:
@@ -105,16 +8,14 @@ public:
 
 	//DLLの初期化
 	//戻り値：
-	// エラーコード
+	// TRUE（成功）、FALSE（失敗）
 	//引数：
 	// loadDllFilePath		[IN]ロードするDLLパス
 	BOOL Initialize(
 		LPCWSTR loadDllFilePath
 		);
 
-	//DLLの開放
-	//戻り値：
-	// エラーコード
+	//DLLの解放
 	void UnInitialize(
 		);
 
@@ -122,11 +23,11 @@ public:
 	//nameがNULL時は必要なサイズをnameSizeで返す
 	//通常nameSize=256で呼び出し
 	//戻り値
-	// TRUE（成功）、FALSE（失敗）
+	// TRUE（成功）、FALSE（失敗）、ERR_NOT_INIT（未初期化）
 	//引数：
 	// name						[OUT]名称
 	// nameSize					[IN/OUT]nameのサイズ(WCHAR単位)
-	BOOL GetPlugInName(
+	BOOL GetName(
 		WCHAR* name,
 		DWORD* nameSize
 		);
@@ -134,18 +35,18 @@ public:
 	//PlugInで設定が必要な場合、設定用のダイアログなどを表示する
 	//引数：
 	// parentWnd				[IN]親ウインドウ
-	void Setting(
+	void ShowSetting(
 		HWND parentWnd
 		);
 
 	//ファイル保存を開始する
 	//戻り値：
-	// TRUE（成功）、FALSE（失敗）
+	// TRUE（成功）、FALSE（失敗）、ERR_NOT_INIT（未初期化）
 	//引数：
 	// fileName				[IN]保存ファイルフルパス（必要に応じて拡張子変えたりなど行う）
 	// overWriteFlag		[IN]同一ファイル名存在時に上書きするかどうか（TRUE：する、FALSE：しない）
 	// createSize			[IN]入力予想容量（188バイトTSでの容量。即時録画時など総時間未定の場合は0。延長などの可能性もあるので目安程度）
-	BOOL StartSave(
+	BOOL Start(
 		LPCWSTR fileName,
 		BOOL overWriteFlag,
 		ULONGLONG createSize
@@ -153,19 +54,19 @@ public:
 
 	//ファイル保存を終了する
 	//戻り値：
-	// TRUE（成功）、FALSE（失敗）
-	BOOL StopSave(
+	// TRUE（成功）、FALSE（失敗）、ERR_NOT_INIT（未初期化）
+	BOOL Stop(
 		);
 
 	//実際に保存しているファイルパスを取得する（再生やバッチ処理に利用される）
 	//filePathがNULL時は必要なサイズをfilePathSizeで返す
 	//通常filePathSize=512で呼び出し
 	//戻り値：
-	// TRUE（成功）、FALSE（失敗）
+	// TRUE（成功）、FALSE（失敗）、ERR_NOT_INIT（未初期化）
 	//引数：
 	// filePath				[OUT]保存ファイルフルパス
 	// filePathSize			[IN/OUT]filePathのサイズ(WCHAR単位)
-	BOOL GetSaveFilePath(
+	BOOL GetSavePath(
 		WCHAR* filePath,
 		DWORD* filePathSize
 		);
@@ -174,18 +75,27 @@ public:
 	//空き容量不足などで書き出し失敗した場合、writeSizeの値を元に
 	//再度保存処理するときの送信開始地点を決める
 	//戻り値：
-	// TRUE（成功）、FALSE（失敗）
+	// TRUE（成功）、FALSE（失敗）、ERR_NOT_INIT（未初期化）
 	//引数：
 	// data					[IN]TSデータ
 	// size					[IN]dataのサイズ
 	// writeSize			[OUT]保存に利用したサイズ
-	BOOL AddTSBuff(
+	BOOL Write(
 		BYTE* data,
 		DWORD size,
 		DWORD* writeSize
 		);
 
 protected:
+	typedef BOOL (WINAPI *GetPlugInNameWP)(WCHAR* name, DWORD* nameSize);
+	typedef void (WINAPI *SettingWP)(HWND parentWnd);
+	typedef BOOL (WINAPI *CreateCtrlWP)(DWORD* id);
+	typedef BOOL (WINAPI *DeleteCtrlWP)(DWORD id);
+	typedef BOOL (WINAPI *StartSaveWP)(DWORD id, LPCWSTR fileName, BOOL overWriteFlag, ULONGLONG createSize);
+	typedef BOOL (WINAPI *StopSaveWP)(DWORD id);
+	typedef BOOL (WINAPI *GetSaveFilePathWP)(DWORD id, WCHAR* filePath, DWORD* filePathSize);
+	typedef BOOL (WINAPI *AddTSBuffWP)(DWORD id, BYTE* data, DWORD size, DWORD* writeSize);
+
 	HMODULE module;
 	DWORD id;
 
