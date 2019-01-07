7345a59a8d2867a9e97250e4d38f9dc5801e8c2d
xtne6f <xt4ubq@gmail.com>
2019-01-07 23:33:13 +0900

    正規表現モードで無意味なグローバルマッチをやめる
    意図不明のため踏襲していたが恐らく無意味なうえ効率もたぶん悪い
    その他些末な最適化

--- EpgTimerSrv/EpgTimerSrv/EpgDBManager.cpp
+++ EpgTimerSrv/EpgTimerSrv/EpgDBManager.cpp
@@ -897,7 +897,6 @@ void CEpgDBManager::AddKeyword(vector<pair<wstring, RegExpPtr>>& keyList, wstrin
 			keyList.back().second.reset((IRegExp*)pv);
 			OleCharPtr pattern(SysAllocString(key.c_str() + 7), SysFreeString);
 			if( pattern &&
-			    SUCCEEDED(keyList.back().second->put_Global(VARIANT_TRUE)) &&
 			    SUCCEEDED(keyList.back().second->put_IgnoreCase(caseFlag ? VARIANT_FALSE : VARIANT_TRUE)) &&
 			    SUCCEEDED(keyList.back().second->put_Pattern(pattern.get())) ){
 				keyList.back().first.swap(key);
@@ -1003,52 +1002,60 @@ void CEpgDBManager::ConvertSearchText(wstring& str)
 {
 	//全角英数およびこのテーブルにある文字列を置換する
 	//最初の文字(UTF-16)をキーとしてソート済み。同一キー内の順序はマッチの優先順
-	static const wchar_t convertFrom[][3] = {
+	static const WCHAR convertFrom[][2] = {
 		L"’", L"”", L"　",
 		L"！", L"＃", L"＄", L"％", L"＆", L"（", L"）", L"＊", L"＋", L"，", L"\xFF0D", L"．", L"／",
 		L"：", L"；", L"＜", L"＝", L"＞", L"？", L"＠", L"［", L"］", L"＾", L"＿", L"｀", L"｛", L"｜", L"｝", L"\xFF5E",
 		L"｡", L"｢", L"｣", L"､", L"･", L"ｦ", L"ｧ", L"ｨ", L"ｩ", L"ｪ", L"ｫ", L"ｬ", L"ｭ", L"ｮ", L"ｯ", L"ｰ", L"ｱ", L"ｲ", L"ｳ", L"ｴ", L"ｵ",
-		L"ｶﾞ", L"ｶ", L"ｷﾞ", L"ｷ", L"ｸﾞ", L"ｸ", L"ｹﾞ", L"ｹ", L"ｺﾞ", L"ｺ",
-		L"ｻﾞ", L"ｻ", L"ｼﾞ", L"ｼ", L"ｽﾞ", L"ｽ", L"ｾﾞ", L"ｾ", L"ｿﾞ", L"ｿ",
-		L"ﾀﾞ", L"ﾀ", L"ﾁﾞ", L"ﾁ", L"ﾂﾞ", L"ﾂ", L"ﾃﾞ", L"ﾃ", L"ﾄﾞ", L"ﾄ",
+		{L'ｶ', L'ﾞ'}, L"ｶ", {L'ｷ', L'ﾞ'}, L"ｷ", {L'ｸ', L'ﾞ'}, L"ｸ", {L'ｹ', L'ﾞ'}, L"ｹ", {L'ｺ', L'ﾞ'}, L"ｺ",
+		{L'ｻ', L'ﾞ'}, L"ｻ", {L'ｼ', L'ﾞ'}, L"ｼ", {L'ｽ', L'ﾞ'}, L"ｽ", {L'ｾ', L'ﾞ'}, L"ｾ", {L'ｿ', L'ﾞ'}, L"ｿ",
+		{L'ﾀ', L'ﾞ'}, L"ﾀ", {L'ﾁ', L'ﾞ'}, L"ﾁ", {L'ﾂ', L'ﾞ'}, L"ﾂ", {L'ﾃ', L'ﾞ'}, L"ﾃ", {L'ﾄ', L'ﾞ'}, L"ﾄ",
 		L"ﾅ", L"ﾆ", L"ﾇ", L"ﾈ", L"ﾉ",
-		L"ﾊﾞ", L"ﾊﾟ", L"ﾊ", L"ﾋﾞ", L"ﾋﾟ", L"ﾋ", L"ﾌﾞ", L"ﾌﾟ", L"ﾌ", L"ﾍﾞ", L"ﾍﾟ", L"ﾍ", L"ﾎﾞ", L"ﾎﾟ", L"ﾎ",
+		{L'ﾊ', L'ﾞ'}, {L'ﾊ', L'ﾟ'}, L"ﾊ", {L'ﾋ', L'ﾞ'}, {L'ﾋ', L'ﾟ'}, L"ﾋ", {L'ﾌ', L'ﾞ'}, {L'ﾌ', L'ﾟ'}, L"ﾌ",
+		{L'ﾍ', L'ﾞ'}, {L'ﾍ', L'ﾟ'}, L"ﾍ", {L'ﾎ', L'ﾞ'}, {L'ﾎ', L'ﾟ'}, L"ﾎ",
 		L"ﾏ", L"ﾐ", L"ﾑ", L"ﾒ", L"ﾓ", L"ﾔ", L"ﾕ", L"ﾖ", L"ﾗ", L"ﾘ", L"ﾙ", L"ﾚ", L"ﾛ", L"ﾜ", L"ﾝ", L"ﾞ", L"ﾟ",
 		L"￥",
 	};
-	static const wchar_t convertTo[][2] = {
-		L"'", L"\"", L" ",
-		L"!", L"#", L"$", L"%", L"&", L"(", L")", L"*", L"+", L",", L"-", L".", L"/",
-		L":", L";", L"<", L"=", L">", L"?", L"@", L"[", L"]", L"^", L"_", L"`", L"{", L"|", L"}", L"~",
-		L"。", L"「", L"」", L"、", L"・", L"ヲ", L"ァ", L"ィ", L"ゥ", L"ェ", L"ォ", L"ャ", L"ュ", L"ョ", L"ッ", L"ー", L"ア", L"イ", L"ウ", L"エ", L"オ",
-		L"ガ", L"カ", L"ギ", L"キ", L"グ", L"ク", L"ゲ", L"ケ", L"ゴ", L"コ",
-		L"ザ", L"サ", L"ジ", L"シ", L"ズ", L"ス", L"ゼ", L"セ", L"ゾ", L"ソ",
-		L"ダ", L"タ", L"ヂ", L"チ", L"ヅ", L"ツ", L"デ", L"テ", L"ド", L"ト",
-		L"ナ", L"ニ", L"ヌ", L"ネ", L"ノ",
-		L"バ", L"パ", L"ハ", L"ビ", L"ピ", L"ヒ", L"ブ", L"プ", L"フ", L"ベ", L"ペ", L"ヘ", L"ボ", L"ポ", L"ホ",
-		L"マ", L"ミ", L"ム", L"メ", L"モ", L"ヤ", L"ユ", L"ヨ", L"ラ", L"リ", L"ル", L"レ", L"ロ", L"ワ", L"ン", L"゛", L"゜",
-		L"\\",
+	static const WCHAR convertTo[] = {
+		L'\'', L'"', L' ',
+		L'!', L'#', L'$', L'%', L'&', L'(', L')', L'*', L'+', L',', L'-', L'.', L'/',
+		L':', L';', L'<', L'=', L'>', L'?', L'@', L'[', L']', L'^', L'_', L'`', L'{', L'|', L'}', L'~',
+		L'。', L'「', L'」', L'、', L'・', L'ヲ', L'ァ', L'ィ', L'ゥ', L'ェ', L'ォ', L'ャ', L'ュ', L'ョ', L'ッ', L'ー', L'ア', L'イ', L'ウ', L'エ', L'オ',
+		L'ガ', L'カ', L'ギ', L'キ', L'グ', L'ク', L'ゲ', L'ケ', L'ゴ', L'コ',
+		L'ザ', L'サ', L'ジ', L'シ', L'ズ', L'ス', L'ゼ', L'セ', L'ゾ', L'ソ',
+		L'ダ', L'タ', L'ヂ', L'チ', L'ヅ', L'ツ', L'デ', L'テ', L'ド', L'ト',
+		L'ナ', L'ニ', L'ヌ', L'ネ', L'ノ',
+		L'バ', L'パ', L'ハ', L'ビ', L'ピ', L'ヒ', L'ブ', L'プ', L'フ',
+		L'ベ', L'ペ', L'ヘ', L'ボ', L'ポ', L'ホ',
+		L'マ', L'ミ', L'ム', L'メ', L'モ', L'ヤ', L'ユ', L'ヨ', L'ラ', L'リ', L'ル', L'レ', L'ロ', L'ワ', L'ン', L'゛', L'゜',
+		L'\\',
 	};
 
-	for( size_t i = 0; i < str.size(); i++ ){
-		if( L'０' <= str[i] && str[i] <= L'９' ){
-			str[i] = str[i] - L'０' + L'0';
-		}else if( L'Ａ' <= str[i] && str[i] <= L'Ｚ' ){
-			str[i] = str[i] - L'Ａ' + L'A';
-		}else if( L'ａ' <= str[i] && str[i] <= L'ｚ' ){
-			str[i] = str[i] - L'ａ' + L'a';
-		}
+	for( wstring::iterator itr = str.begin(), itrEnd = str.end(); itr != itrEnd; itr++ ){
 		//注意: これは符号位置の連続性を利用してテーブル参照を減らすための条件。上記のテーブルを弄る場合はここを確認すること
-		else if( str[i] == L'’' || str[i] == L'”' || str[i] == L'　' || L'！' <= str[i] && str[i] <= L'￥' ){
-			const wchar_t (*f)[3] = std::lower_bound(convertFrom, convertFrom + _countof(convertFrom), &str[i],
-			                                         [](const wchar_t* a, const wchar_t* b) { return (unsigned int)a[0] < (unsigned int)b[0]; });
-			for( ; f != convertFrom + _countof(convertFrom) && (*f)[0] == str[i]; f++ ){
-				if( (*f)[1] == L'\0' ){
-					str.replace(i, 1, convertTo[f - convertFrom]);
-					break;
-				}else if( i + 1 < str.size() && str[i + 1] == (*f)[1] ){
-					str.replace(i, 2, convertTo[f - convertFrom]);
-					break;
+		WCHAR c = *itr;
+		if( (L'！' <= c && c <= L'￥') || c == L'　' || c == L'’' || c == L'”' ){
+			if( L'０' <= c && c <= L'９' ){
+				*itr = c - L'０' + L'0';
+			}else if( L'Ａ' <= c && c <= L'Ｚ' ){
+				*itr = c - L'Ａ' + L'A';
+			}else if( L'ａ' <= c && c <= L'ｚ' ){
+				*itr = c - L'ａ' + L'a';
+			}else{
+				const WCHAR (*f)[2] = std::lower_bound(convertFrom, convertFrom + _countof(convertFrom), &*itr,
+				                                       [](LPCWSTR a, LPCWSTR b) { return (unsigned short)a[0] < (unsigned short)b[0]; });
+				for( ; f != convertFrom + _countof(convertFrom) && (*f)[0] == c; f++ ){
+					if( (*f)[1] == L'\0' ){
+						*itr = convertTo[f - convertFrom];
+						break;
+					}else if( itr + 1 != itrEnd && *(itr + 1) == (*f)[1] ){
+						size_t i = itrEnd - itr - 1;
+						str.replace(itr, itr + 2, 1, convertTo[f - convertFrom]);
+						//イテレータを再有効化
+						itrEnd = str.end();
+						itr = itrEnd - i;
+						break;
+					}
 				}
 			}
 		}
