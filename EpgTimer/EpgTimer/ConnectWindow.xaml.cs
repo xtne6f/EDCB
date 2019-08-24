60176e3e22832d86d641ef5fa15e486974a9ede1
tkntrec <Tokinata@gmail.com>
2019-08-25 00:37:48 +0900

    マージミスを修正
    「マジックパケット送信で複数ネットワークI/Fに対応(2ch_EDCB/68/693～)」( xtne6f@4ab0ce4 )をマージする際、マジックパケット送信後のmacアドレス保存を削除してしまっていた。

--- EpgTimer/EpgTimer/ConnectWindow.xaml.cs
+++ EpgTimer/EpgTimer/ConnectWindow.xaml.cs
@@ -69,6 +69,7 @@ namespace EpgTimer
                 if (macAddress != null)
                 {
                     NWConnect.SendMagicPacket(macAddress);
+                    Settings.Instance.NWMacAdd = textBox_mac.Text;
                     return;
                 }
             }
