407cf56251ca7acc048966e85949d7adc9fb4508
kouhgithub <73493189+kouhgithub@users.noreply.github.com>
2023-04-12 20:17:28 +0900

    文字化け修正

--- Common/StructDef.h
+++ Common/StructDef.h
@@ -3,33 +3,33 @@
 
 #include "Util.h"
 
-//�^��t�H���_���
+//録画フォルダ情報
 typedef struct _REC_FILE_SET_INFO{
-	wstring recFolder;			//�^��t�H���_
-	wstring writePlugIn;		//�o��PlugIn
-	wstring recNamePlugIn;		//�t�@�C�����ϊ�PlugIn�̎g�p
-	wstring recFileName;		//�t�@�C�����ʑΉ� �^��J�n�������ɓ����Ŏg�p�B�\����Ƃ��Ă͕K�v�Ȃ�
+	wstring recFolder;			//録画フォルダ
+	wstring writePlugIn;		//出力PlugIn
+	wstring recNamePlugIn;		//ファイル名変換PlugInの使用
+	wstring recFileName;		//ファイル名個別対応 録画開始処理時に内部で使用。予約情報としては必要なし
 }REC_FILE_SET_INFO;
 
-//�^��ݒ���
+//録画設定情報
 typedef struct _REC_SETTING_DATA{
-	BYTE recMode;				//�^�惂�[�h
-	BYTE priority;				//�D��x
-	BYTE tuijyuuFlag;			//�Ǐ]���[�h
-	DWORD serviceMode;			//�����Ώۃf�[�^���[�h
-	BYTE pittariFlag;			//�҂�����H�^��
-	wstring batFilePath;		//�^���BAT�t�@�C���p�X
-	vector<REC_FILE_SET_INFO> recFolderList;		//�^��t�H���_�p�X
-	BYTE suspendMode;			//�x�~���[�h
-	BYTE rebootFlag;			//�^���ċN������
-	BYTE useMargineFlag;		//�^��}�[�W�����ʎw��
-	INT startMargine;			//�^��J�n���̃}�[�W��
-	INT endMargine;				//�^��I�����̃}�[�W��
-	BYTE continueRecFlag;		//�㑱����T�[�r�X���A����t�@�C���Ř^��
-	BYTE partialRecFlag;		//����CH�ɕ�����M�T�[�r�X������ꍇ�A�����^�悷�邩�ǂ���
-	DWORD tunerID;				//�����I�Ɏg�pTuner���Œ�
-	//CMD_VER 2�ȍ~
-	vector<REC_FILE_SET_INFO> partialRecFolder;	//������M�T�[�r�X�^��̃t�H���_
+	BYTE recMode;				//録画モード
+	BYTE priority;				//優先度
+	BYTE tuijyuuFlag;			//追従モード
+	DWORD serviceMode;			//処理対象データモード
+	BYTE pittariFlag;			//ぴったり？録画
+	wstring batFilePath;		//録画後BATファイルパス
+	vector<REC_FILE_SET_INFO> recFolderList;		//録画フォルダパス
+	BYTE suspendMode;			//休止モード
+	BYTE rebootFlag;			//録画後再起動する
+	BYTE useMargineFlag;		//録画マージンを個別指定
+	INT startMargine;			//録画開始時のマージン
+	INT endMargine;				//録画終了時のマージン
+	BYTE continueRecFlag;		//後続同一サービス時、同一ファイルで録画
+	BYTE partialRecFlag;		//物理CHに部分受信サービスがある場合、同時録画するかどうか
+	DWORD tunerID;				//強制的に使用Tunerを固定
+	//CMD_VER 2以降
+	vector<REC_FILE_SET_INFO> partialRecFolder;	//部分受信サービス録画のフォルダ
 	_REC_SETTING_DATA(void){
 		recMode = 1;
 		priority = 1;
@@ -48,27 +48,27 @@ typedef struct _REC_SETTING_DATA{
 	};
 } REC_SETTING_DATA;
 
-//�o�^�\����
+//登録予約情報
 typedef struct _RESERVE_DATA{
-	wstring title;					//�ԑg��
-	SYSTEMTIME startTime;			//�^��J�n����
-	DWORD durationSecond;			//�^�摍����
-	wstring stationName;			//�T�[�r�X��
+	wstring title;					//番組名
+	SYSTEMTIME startTime;			//録画開始時間
+	DWORD durationSecond;			//録画総時間
+	wstring stationName;			//サービス名
 	WORD originalNetworkID;			//ONID
 	WORD transportStreamID;			//TSID
 	WORD serviceID;					//SID
 	WORD eventID;					//EventID
-	wstring comment;				//�R�����g
-	DWORD reserveID;				//�\�񎯕�ID �\��o�^����0
-	BYTE recWaitFlag;				//�\��ҋ@�������H �����Ŏg�p
-	BYTE overlapMode;				//���Ԃ��� 1:���Ԃ��ă`���[�i�[����Ȃ��\�񂠂� 2:�`���[�i�[����Ȃ��ė\��ł��Ȃ�
-	wstring recFilePath;			//�^��t�@�C���p�X ���o�[�W�����݊��p ���g�p
-	SYSTEMTIME startTimeEpg;		//�\�񎞂̊J�n����
-	REC_SETTING_DATA recSetting;	//�^��ݒ�
-	DWORD reserveStatus;			//�\��ǉ���� �����Ŏg�p
-	//CMD_VER 5�ȍ~
-	vector<wstring> recFileNameList;	//�^��\��t�@�C����
-	DWORD param1;					//�����p
+	wstring comment;				//コメント
+	DWORD reserveID;				//予約識別ID 予約登録時は0
+	BYTE recWaitFlag;				//予約待機入った？ 内部で使用
+	BYTE overlapMode;				//かぶり状態 1:かぶってチューナー足りない予約あり 2:チューナー足りなくて予約できない
+	wstring recFilePath;			//録画ファイルパス 旧バージョン互換用 未使用
+	SYSTEMTIME startTimeEpg;		//予約時の開始時間
+	REC_SETTING_DATA recSetting;	//録画設定
+	DWORD reserveStatus;			//予約追加状態 内部で使用
+	//CMD_VER 5以降
+	vector<wstring> recFileNameList;	//録画予定ファイル名
+	DWORD param1;					//将来用
 	_RESERVE_DATA(void){
 		title=L"";
 		ZeroMemory(&startTime, sizeof(SYSTEMTIME));
@@ -91,23 +91,23 @@ typedef struct _RESERVE_DATA{
 
 typedef struct _REC_FILE_INFO{
 	DWORD id;					//ID
-	wstring recFilePath;		//�^��t�@�C���p�X
-	wstring title;				//�ԑg��
-	SYSTEMTIME startTime;		//�J�n����
-	DWORD durationSecond;		//�^�掞��
-	wstring serviceName;		//�T�[�r�X��
+	wstring recFilePath;		//録画ファイルパス
+	wstring title;				//番組名
+	SYSTEMTIME startTime;		//開始時間
+	DWORD durationSecond;		//録画時間
+	wstring serviceName;		//サービス名
 	WORD originalNetworkID;		//ONID
 	WORD transportStreamID;		//TSID
 	WORD serviceID;				//SID
 	WORD eventID;				//EventID
-	__int64 drops;				//�h���b�v��
-	__int64 scrambles;			//�X�N�����u����
-	DWORD recStatus;			//�^�挋�ʂ̃X�e�[�^�X
-	SYSTEMTIME startTimeEpg;	//�\�񎞂̊J�n����
-	wstring comment;			//�R�����g
-	wstring programInfo;		//.program.txt�t�@�C���̓��e
-	wstring errInfo;			//.err�t�@�C���̓��e
-	//CMD_VER 4�ȍ~
+	__int64 drops;				//ドロップ数
+	__int64 scrambles;			//スクランブル数
+	DWORD recStatus;			//録画結果のステータス
+	SYSTEMTIME startTimeEpg;	//予約時の開始時間
+	wstring comment;			//コメント
+	wstring programInfo;		//.program.txtファイルの内容
+	wstring errInfo;			//.errファイルの内容
+	//CMD_VER 4以降
 	BYTE protectFlag;
 	_REC_FILE_INFO(void){
 		id = 0;
@@ -158,20 +158,20 @@ typedef struct _TUNER_RESERVE_INFO{
 	vector<DWORD> reserveList;
 } TUNER_RESERVE_INFO;
 
-//�`���[�i�[���T�[�r�X���
+//チューナー毎サービス情報
 typedef struct _CH_DATA4{
-	int space;						//�`���[�i�[���
-	int ch;							//�����`�����l��
+	int space;						//チューナー空間
+	int ch;							//物理チャンネル
 	WORD originalNetworkID;			//ONID
 	WORD transportStreamID;			//TSID
-	WORD serviceID;					//�T�[�r�XID
-	WORD serviceType;				//�T�[�r�X�^�C�v
-	BOOL partialFlag;				//������M�T�[�r�X�i�����Z�O�j���ǂ���
-	BOOL useViewFlag;				//�ꗗ�\���Ɏg�p���邩�ǂ���
-	wstring serviceName;			//�T�[�r�X��
-	wstring chName;					//�`�����l����
+	WORD serviceID;					//サービスID
+	WORD serviceType;				//サービスタイプ
+	BOOL partialFlag;				//部分受信サービス（ワンセグ）かどうか
+	BOOL useViewFlag;				//一覧表示に使用するかどうか
+	wstring serviceName;			//サービス名
+	wstring chName;					//チャンネル名
 	wstring networkName;			//ts_name or network_name
-	BYTE remoconID;					//�����R��ID
+	BYTE remoconID;					//リモコンID
 	_CH_DATA4(void){
 		space = 0;
 		ch = 0;
@@ -188,17 +188,17 @@ typedef struct _CH_DATA4{
 	};
 } CH_DATA4;
 
-//�S�`���[�i�[�ŔF�������T�[�r�X�ꗗ
+//全チューナーで認識したサービス一覧
 typedef struct _CH_DATA5{
 	WORD originalNetworkID;			//ONID
 	WORD transportStreamID;			//TSID
-	WORD serviceID;					//�T�[�r�XID
-	WORD serviceType;				//�T�[�r�X�^�C�v
-	BOOL partialFlag;				//������M�T�[�r�X�i�����Z�O�j���ǂ���
-	wstring serviceName;			//�T�[�r�X��
+	WORD serviceID;					//サービスID
+	WORD serviceType;				//サービスタイプ
+	BOOL partialFlag;				//部分受信サービス（ワンセグ）かどうか
+	wstring serviceName;			//サービス名
 	wstring networkName;			//ts_name or network_name
-	BOOL epgCapFlag;				//EPG�f�[�^�擾�Ώۂ��ǂ���
-	BOOL searchFlag;				//�������̃f�t�H���g�����ΏۃT�[�r�X���ǂ���
+	BOOL epgCapFlag;				//EPGデータ取得対象かどうか
+	BOOL searchFlag;				//検索時のデフォルト検索対象サービスかどうか
 	_CH_DATA5(void){
 		originalNetworkID = 0;
 		transportStreamID = 0;
@@ -217,11 +217,11 @@ typedef struct _REGIST_TCP_INFO{
 	DWORD port;
 }REGIST_TCP_INFO;
 
-//�R�}���h����M�X�g���[��
+//コマンド送受信ストリーム
 typedef struct _CMD_STREAM{
-	DWORD param;	//���M���R�}���h�A��M���G���[�R�[�h
-	DWORD dataSize;	//data�̃T�C�Y�iBYTE�P�ʁj
-	BYTE* data;		//����M����o�C�i���f�[�^
+	DWORD param;	//送信時コマンド、受信時エラーコード
+	DWORD dataSize;	//dataのサイズ（BYTE単位）
+	BYTE* data;		//送受信するバイナリデータ
 	_CMD_STREAM(void){
 		param = 0;
 		dataSize = 0;
@@ -236,9 +236,9 @@ private:
 } CMD_STREAM;
 
 typedef struct _HTTP_STREAM{
-	string httpHeader;	//���M���R�}���h�A��M���G���[�R�[�h
-	DWORD dataSize;	//data�̃T�C�Y�iBYTE�P�ʁj
-	BYTE* data;		//����M����o�C�i���f�[�^
+	string httpHeader;	//送信時コマンド、受信時エラーコード
+	DWORD dataSize;	//dataのサイズ（BYTE単位）
+	BYTE* data;		//送受信するバイナリデータ
 	_HTTP_STREAM(void){
 		httpHeader = "";
 		dataSize = 0;
@@ -252,18 +252,18 @@ private:
 	_HTTP_STREAM & operator= (const _HTTP_STREAM &);
 } HTTP_STREAM;
 
-//EPG��{���
+//EPG基本情報
 typedef struct _EPGDB_SHORT_EVENT_INFO{
-	wstring event_name;			//�C�x���g��
-	wstring text_char;			//���
+	wstring event_name;			//イベント名
+	wstring text_char;			//情報
 } EPGDB_SHORT_EVENT_INFO;
 
-//EPG�g�����
+//EPG拡張情報
 typedef struct _EPGDB_EXTENDED_EVENT_INFO{
-	wstring text_char;			//�ڍ׏��
+	wstring text_char;			//詳細情報
 } EPGDB_EXTENDED_EVENT_INFO;
 
-//EPG�W�������f�[�^
+//EPGジャンルデータ
 typedef struct _EPGDB_CONTENT_DATA{
 	BYTE content_nibble_level_1;
 	BYTE content_nibble_level_2;
@@ -271,20 +271,20 @@ typedef struct _EPGDB_CONTENT_DATA{
 	BYTE user_nibble_2;
 }EPGDB_CONTENT_DATA;
 
-//EPG�W���������
+//EPGジャンル情報
 typedef struct _EPGDB_CONTENT_INFO{
 	vector<EPGDB_CONTENT_DATA> nibbleList;
 } EPGDB_CONTEN_INFO;
 
-//EPG�f�����
+//EPG映像情報
 typedef struct _EPGDB_COMPONENT_INFO{
 	BYTE stream_content;
 	BYTE component_type;
 	BYTE component_tag;
-	wstring text_char;			//���
+	wstring text_char;			//情報
 } EPGDB_COMPONENT_INFO;
 
-//EPG�������f�[�^
+//EPG音声情報データ
 typedef struct _EPGDB_AUDIO_COMPONENT_INFO_DATA{
 	BYTE stream_content;
 	BYTE component_type;
@@ -295,15 +295,15 @@ typedef struct _EPGDB_AUDIO_COMPONENT_INFO_DATA{
 	BYTE main_component_flag;
 	BYTE quality_indicator;
 	BYTE sampling_rate;
-	wstring text_char;			//�ڍ׏��
+	wstring text_char;			//詳細情報
 } EPGDB_AUDIO_COMPONENT_INFO_DATA;
 
-//EPG�������
+//EPG音声情報
 typedef struct _EPGDB_AUDIO_COMPONENT_INFO{
 	vector<EPGDB_AUDIO_COMPONENT_INFO_DATA> componentList;
 } EPGDB_AUDIO_COMPONENT_INFO;
 
-//EPG�C�x���g�f�[�^
+//EPGイベントデータ
 typedef struct _EPGDB_EVENT_DATA{
 	WORD original_network_id;
 	WORD transport_stream_id;
@@ -311,7 +311,7 @@ typedef struct _EPGDB_EVENT_DATA{
 	WORD event_id;
 }EPGDB_EVENT_DATA;
 
-//EPG�C�x���g�O���[�v���
+//EPGイベントグループ情報
 typedef struct _EPGDB_EVENTGROUP_INFO{
 	BYTE group_type;
 	vector<EPGDB_EVENT_DATA> eventDataList;
@@ -321,21 +321,21 @@ typedef struct _EPGDB_EVENT_INFO{
 	WORD original_network_id;
 	WORD transport_stream_id;
 	WORD service_id;
-	WORD event_id;							//�C�x���gID
-	BYTE StartTimeFlag;						//start_time�̒l���L�����ǂ���
-	SYSTEMTIME start_time;					//�J�n����
-	BYTE DurationFlag;						//duration�̒l���L�����ǂ���
-	DWORD durationSec;						//�����ԁi�P�ʁF�b�j
+	WORD event_id;							//イベントID
+	BYTE StartTimeFlag;						//start_timeの値が有効かどうか
+	SYSTEMTIME start_time;					//開始時間
+	BYTE DurationFlag;						//durationの値が有効かどうか
+	DWORD durationSec;						//総時間（単位：秒）
 
-	EPGDB_SHORT_EVENT_INFO* shortInfo;		//��{���
-	EPGDB_EXTENDED_EVENT_INFO* extInfo;		//�g�����
-	EPGDB_CONTEN_INFO* contentInfo;			//�W���������
-	EPGDB_COMPONENT_INFO* componentInfo;		//�f�����
-	EPGDB_AUDIO_COMPONENT_INFO* audioInfo;	//�������
-	EPGDB_EVENTGROUP_INFO* eventGroupInfo;	//�C�x���g�O���[�v���
-	EPGDB_EVENTGROUP_INFO* eventRelayInfo;	//�C�x���g�����[���
+	EPGDB_SHORT_EVENT_INFO* shortInfo;		//基本情報
+	EPGDB_EXTENDED_EVENT_INFO* extInfo;		//拡張情報
+	EPGDB_CONTEN_INFO* contentInfo;			//ジャンル情報
+	EPGDB_COMPONENT_INFO* componentInfo;		//映像情報
+	EPGDB_AUDIO_COMPONENT_INFO* audioInfo;	//音声情報
+	EPGDB_EVENTGROUP_INFO* eventGroupInfo;	//イベントグループ情報
+	EPGDB_EVENTGROUP_INFO* eventRelayInfo;	//イベントリレー情報
 
-	BYTE freeCAFlag;						//�m���X�N�����u���t���O
+	BYTE freeCAFlag;						//ノンスクランブルフラグ
 	_EPGDB_EVENT_INFO(void){
 		shortInfo = NULL;
 		extInfo = NULL;
@@ -423,7 +423,7 @@ typedef struct _EPGDB_SEARCH_DATE_INFO{
 	WORD endMin;
 } EPGDB_SEARCH_DATE_INFO;
 
-//��������
+//検索条件
 typedef struct _EPGDB_SEARCH_KEY_INFO{
 	wstring andKey;
 	wstring notKey;
@@ -438,10 +438,10 @@ typedef struct _EPGDB_SEARCH_KEY_INFO{
 	BYTE notContetFlag;
 	BYTE notDateFlag;
 	BYTE freeCAFlag;
-	//CMD_VER 3�ȍ~
-	//�����\��o�^�̏�����p
-	BYTE chkRecEnd;					//�^��ς��̃`�F�b�N����
-	WORD chkRecDay;					//�^��ς��̃`�F�b�N�Ώۊ���
+	//CMD_VER 3以降
+	//自動予約登録の条件専用
+	BYTE chkRecEnd;					//録画済かのチェックあり
+	WORD chkRecDay;					//録画済かのチェック対象期間
 	_EPGDB_SEARCH_KEY_INFO(void){
 		andKey = L"";
 		notKey = L"";
@@ -456,35 +456,35 @@ typedef struct _EPGDB_SEARCH_KEY_INFO{
 	};
 }EPGDB_SEARCH_KEY_INFO;
 
-//�����\��o�^���
+//自動予約登録情報
 typedef struct _EPG_AUTO_ADD_DATA{
 	DWORD dataID;
-	EPGDB_SEARCH_KEY_INFO searchInfo;	//�����L�[
-	REC_SETTING_DATA recSetting;	//�^��ݒ�
-	DWORD addCount;		//�\��o�^��
+	EPGDB_SEARCH_KEY_INFO searchInfo;	//検索キー
+	REC_SETTING_DATA recSetting;	//録画設定
+	DWORD addCount;		//予約登録数
 } EPG_AUTO_ADD_DATA;
 
 typedef struct _MANUAL_AUTO_ADD_DATA{
 	DWORD dataID;
-	BYTE dayOfWeekFlag;				//�Ώۗj��
-	DWORD startTime;				//�^��J�n���ԁi00:00��0�Ƃ��ĕb�P�ʁj
-	DWORD durationSecond;			//�^�摍����
-	wstring title;					//�ԑg��
-	wstring stationName;			//�T�[�r�X��
+	BYTE dayOfWeekFlag;				//対象曜日
+	DWORD startTime;				//録画開始時間（00:00を0として秒単位）
+	DWORD durationSecond;			//録画総時間
+	wstring title;					//番組名
+	wstring stationName;			//サービス名
 	WORD originalNetworkID;			//ONID
 	WORD transportStreamID;			//TSID
 	WORD serviceID;					//SID
-	REC_SETTING_DATA recSetting;	//�^��ݒ�
+	REC_SETTING_DATA recSetting;	//録画設定
 } MANUAL_AUTO_ADD_DATA;
 
-//�R�}���h���M�p
-//�`�����l���ύX���
+//コマンド送信用
+//チャンネル変更情報
 typedef struct _SET_CH_INFO{
-	BOOL useSID;//wONID��wTSID��wSID�̒l���g�p�ł��邩�ǂ���
+	BOOL useSID;//wONIDとwTSIDとwSIDの値が使用できるかどうか
 	WORD ONID;
 	WORD TSID;
 	WORD SID;
-	BOOL useBonCh;//dwSpace��dwCh�̒l���g�p�ł��邩�ǂ���
+	BOOL useBonCh;//dwSpaceとdwChの値が使用できるかどうか
 	DWORD space;
 	DWORD ch;
 }SET_CH_INFO;
@@ -559,14 +559,14 @@ typedef struct _NWPLAY_PLAY_INFO{
 	DWORD ip;
 	BYTE udp;
 	BYTE tcp;
-	DWORD udpPort;//out�Ŏ��ۂ̊J�n�|�[�g
-	DWORD tcpPort;//out�Ŏ��ۂ̊J�n�|�[�g
+	DWORD udpPort;//outで実際の開始ポート
+	DWORD tcpPort;//outで実際の開始ポート
 } NWPLAY_PLAY_INFO;
 
 typedef struct _NWPLAY_POS_CMD{
 	DWORD ctrlID;
 	__int64 currentPos;
-	__int64 totalPos;//CMD2_EPG_SRV_NWPLAY_SET_POS���͖���
+	__int64 totalPos;//CMD2_EPG_SRV_NWPLAY_SET_POS時は無視
 } NWPLAY_POS_CMD;
 
 typedef struct _NWPLAY_TIMESHIFT_INFO{
@@ -574,16 +574,16 @@ typedef struct _NWPLAY_TIMESHIFT_INFO{
 	wstring filePath;
 } NWPLAY_TIMESHIFT_INFO;
 
-//���ʒm�p�p�����[�^�[
+//情報通知用パラメーター
 typedef struct _NOTIFY_SRV_INFO{
-	DWORD notifyID;		//�ʒm���̎��
-	SYSTEMTIME time;	//�ʒm��Ԃ̔�����������
-	DWORD param1;		//�p�����[�^�[�P�i��ނɂ���ē��e�ύX�j
-	DWORD param2;		//�p�����[�^�[�Q�i��ނɂ���ē��e�ύX�j
-	DWORD param3;		//�p�����[�^�[�R�i��ނɂ���ē��e�ύX�j
-	wstring param4;		//�p�����[�^�[�S�i��ނɂ���ē��e�ύX�j
-	wstring param5;		//�p�����[�^�[�T�i��ނɂ���ē��e�ύX�j
-	wstring param6;		//�p�����[�^�[�U�i��ނɂ���ē��e�ύX�j
+	DWORD notifyID;		//通知情報の種類
+	SYSTEMTIME time;	//通知状態の発生した時間
+	DWORD param1;		//パラメーター１（種類によって内容変更）
+	DWORD param2;		//パラメーター２（種類によって内容変更）
+	DWORD param3;		//パラメーター３（種類によって内容変更）
+	wstring param4;		//パラメーター４（種類によって内容変更）
+	wstring param5;		//パラメーター５（種類によって内容変更）
+	wstring param6;		//パラメーター６（種類によって内容変更）
 	_NOTIFY_SRV_INFO(void){
 		notifyID= 0;
 		param1 = 0;
@@ -591,7 +591,6 @@ typedef struct _NOTIFY_SRV_INFO{
 		param3 = 0;
 	};
 } NOTIFY_SRV_INFO;
-
 
 
 typedef struct _GENRU_INFO{
@@ -609,7 +608,7 @@ typedef struct _GENRU_INFO{
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-//���o�[�W�����R�}���h���M�p
+//旧バージョンコマンド送信用
 typedef struct _OLD_RESERVE_DATA{
 	wstring strTitle;
 	SYSTEMTIME StartTime;
@@ -625,9 +624,9 @@ typedef struct _OLD_RESERVE_DATA{
 	DWORD dwRecMode;
 	BOOL bPittari;
 	wstring strBatPath;
-	DWORD dwReserveID; //����ԑg���ʗpID
-	BOOL bSetWait; //�\��ҋ@�������H
-	DWORD dwPiledUpMode; //���Ԃ��� 1:���Ԃ��ă`���[�i�[����Ȃ��\�񂠂� 2:�`���[�i�[����Ȃ��ė\��ł��Ȃ�
+	DWORD dwReserveID; //同一番組判別用ID
+	BOOL bSetWait; //予約待機入った？
+	DWORD dwPiledUpMode; //かぶり状態 1:かぶってチューナー足りない予約あり 2:チューナー足りなくて予約できない
 	wstring strRecFolder;
 	WORD wSuspendMode;
 	BOOL bReboot;
@@ -688,8 +687,8 @@ typedef struct _OLD_SEARCH_KEY{
 	BOOL bChkSat;
 	BOOL bChkSun;
 	vector<__int64> CHIDList; //ONID<<24 | TSID<<16 | SID
-	//�ȉ������\��o�^���֌W�̂ݎg�p
-	int iAutoAddID; //�����\��o�^�ꗗ�̎��ʗp�L�[
+	//以下自動予約登録時関係のみ使用
+	int iAutoAddID; //自動予約登録一覧の識別用キー
 	int iPriority;
 	int iTuijyuu;
 	int iRecMode;
